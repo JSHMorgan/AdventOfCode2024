c40728dc2203803f5d301f543c9e10650bf4b4c9
Jack Morgan <Jack.Morgan@mmu.ac.uk>
2024-12-04 16:30:48 +0000

    Implemented Word Search class & partial implementation of solver class.

--- Day4/Program.cs
+++ Day4/Program.cs
@@ -1,9 +1,89 @@
-﻿namespace Day4;
+﻿using System.Text;
+
+namespace Day4;
 
 internal class Program
-{
-    static void Main(string[] args)
+{ 
+    private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        string inputData = File.ReadAllText($@"{Environment.CurrentDirectory}/data/test_input.txt");
+        string[] inputArray = inputData.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+        char[,] inputChars = ConvertStringArrayToChars(inputArray);
+        Wordsearch wordSearch = new(inputChars);
+        WordSearchSolver solver = new(wordSearch, "XMAS");
+        int counter = solver.GetWordCount();
+        Console.WriteLine(counter);
+    }
+
+    private static char[,] ConvertStringArrayToChars(string[] inputArray)
+    {
+        char[,] chars = new char[inputArray.Length, inputArray[0].Length];
+        for (int i = 0; i < inputArray.Length; i++)
+        {
+            string item = inputArray[i];
+            for (int j = 0; j < item.Length; j++)
+            {
+                chars[i, j] = item[j];
+            }
+        }
+        return chars;
+    }
+
+    private static bool CheckDown(int index, char[] inputRow)
+    {
+        return CheckRight(index, inputRow);
+    }
+
+    private static bool CheckRight(int index, char[] inputRow)
+    {
+        if (index + 3 >= inputRow.Length)
+        {
+            return false;
+        }
+        StringBuilder builder = new();
+        for (int i = index; i < index + 4; i++)
+        {
+            builder.Append(inputRow[i]);
+        }
+        return builder.Equals("XMAS");
+    }
+
+    private static bool CheckUp(int index, char[] inputRow)
+    {
+        return CheckLeft(index, inputRow);
+    }
+
+    private static bool CheckLeft(int index, char[] inputRow)
+    {
+        if (index - 3 < 0)
+        {
+            return false;
+        }
+        StringBuilder builder = new();
+        for (int i = index - 3; i < index + 1; i++)
+        {
+            builder.Append(inputRow[i]);
+        }
+        return builder.Equals("XMAS");
+    }
+
+    private static bool CheckTopLeftDiagonal(int index, char[] inputDiagonal)
+    {
+        return CheckLeft(index, inputDiagonal);
+    }
+
+    private static bool CheckBottomRightDiagonal(int index, char[] inputDiagonal)
+    {
+        return CheckRight(index, inputDiagonal);
+    }
+
+    private static bool CheckBottomLeftDiagonal(int index, char[] inputDiagonal)
+    {
+        return CheckRight(index, inputDiagonal);
+    }
+
+    private static bool CheckTopRightDiagonal(int index, char[] inputDiagonal)
+    {
+        return CheckRight(index, inputDiagonal);
     }
 }
---
+++ Day4/WordSearchSolver.cs
@@ -0,0 +1,124 @@
+﻿using System.Text;
+using System;
+
+namespace Day4;
+
+internal record WordSearchSolver(Wordsearch Search, string Word)
+{
+    public int GetWordCount()
+    {
+        int counter = 0;
+
+        for (int i = 0; i < Search.RowLength; i++)
+        {
+            counter += GetRowWordCount(i);
+        }
+
+        for (int i = 0; i < Search.ColumnLength; i++)
+        {
+            counter += GetColumnWordCount(i);
+        }
+        
+        for (int i = 0; i < Search.DiagonalLength; i++)
+        {
+            counter += GetTopLeftDiagonalWordCount(i, i);
+        }
+
+        for (int i = 0; i < Search.DiagonalLength; i++)
+        {
+            counter += GetTopRightDiagonalWordCount(i, Search.DiagonalLength - 1 - i);
+        }
+        return counter;
+    }
+    public int GetRowWordCount(int row)
+    {
+        char[] reverseWordArray = Word.ToCharArray();
+        Array.Reverse(reverseWordArray);
+        string reverseWord = new(reverseWordArray);
+
+        int counter = 0;
+        char[] inputRow = Search.GetRow(row);
+        for (int column = 0; column < Search.RowLength - Word.Length + 1; column++)
+        {
+            StringBuilder builder = new();
+            for (int i = 0; i < Word.Length; i++)
+            {
+                _ = builder.Append(inputRow[column + i]);
+            }
+            if (builder.ToString() == Word || builder.ToString() == reverseWord)
+            {
+                counter++;
+            }
+        }
+        return counter;
+    }
+
+    public int GetColumnWordCount(int column)
+    {
+        char[] reverseWordArray = Word.ToCharArray();
+        Array.Reverse(reverseWordArray);
+        string reverseWord = new(reverseWordArray);
+
+        int counter = 0;
+        char[] inputRow = Search.GetColumn(column);
+        for (int row = 0; row < Search.RowLength - Word.Length + 1; row++)
+        {
+            StringBuilder builder = new();
+            for (int i = 0; i < Word.Length; i++)
+            {
+                builder.Append(inputRow[row + i]);
+            }
+            if (builder.ToString() == Word || builder.ToString() == reverseWord)
+            {
+                counter++;
+            }
+        }
+        return counter;
+    }
+
+    public int GetTopLeftDiagonalWordCount(int row, int column)
+    {
+        char[] reverseWordArray = Word.ToCharArray();
+        Array.Reverse(reverseWordArray);
+        string reverseWord = new(reverseWordArray);
+
+        int counter = 0;
+        char[] inputDiagonal = Search.GetBottomLeftDiagonalFromLetter(row, column);
+        for (int diagonal = 0; diagonal < inputDiagonal.Length - Word.Length + 1; diagonal++)
+        {
+            StringBuilder builder = new();
+            for (int i = 0; i < Word.Length; i++)
+            {
+                builder.Append(inputDiagonal[diagonal + i]);
+            }
+            if (builder.ToString() == Word || builder.ToString() == reverseWord)
+            {
+                counter++;
+            }
+        }
+        return counter;
+    }
+
+    public int GetTopRightDiagonalWordCount(int row, int column)
+    {
+        char[] reverseWordArray = Word.ToCharArray();
+        Array.Reverse(reverseWordArray);
+        string reverseWord = new(reverseWordArray);
+
+        int counter = 0;
+        char[] inputDiagonal = Search.GetTopLeftDiagonalFromLetter(row, column);
+        for (int diagonal = 0; diagonal < inputDiagonal.Length - Word.Length + 1; diagonal++)
+        {
+            StringBuilder builder = new();
+            for (int i = 0; i < Word.Length; i++)
+            {
+                builder.Append(inputDiagonal[diagonal + i]);
+            }
+            if (builder.ToString() == Word || builder.ToString() == reverseWord)
+            {
+                counter++;
+            }
+        }
+        return counter;
+    }
+}
