7e2d133b7a5d8c4ada9f117c9ca0868e27c91823
Jack Morgan <Jack.Morgan@mmu.ac.uk>
2024-12-04 10:53:06 +0000

    Completed Day 3

--- DayThree/Program.cs
+++ DayThree/Program.cs
@@ -1,4 +1,5 @@
-﻿using static System.Net.Mime.MediaTypeNames;
+﻿using System.Text.RegularExpressions;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Day3;
 
@@ -7,39 +8,54 @@ internal class Program
     static void Main(string[] args)
     {
         // Split the string at do() and don't() then concat the string.
-        string[] textSplit = File.ReadAllText($@"{Environment.CurrentDirectory}/data/input.txt")
-            .Split("do()");
+        string inputData = File.ReadAllText($@"{Environment.CurrentDirectory}/data/input.txt");
 
+        ExerciseOne(inputData);
+        ExerciseTwo(inputData);
+    }
 
-        var textQuery =
-            from text in textSplit
-            where text.StartsWith('(')
-            select text;
+    private static void ExerciseOne(string inputData)
+    {
+        IEnumerable<string> mulStrings = GetMulStrings(inputData);
 
+        int total = GetTotal(mulStrings);
+        Console.WriteLine(total);
+    }
+    private static void ExerciseTwo(string inputData)
+    {
+        const string pattern = @"(?=do\(\)|don't\(\))";
+        string[] result = Regex.Split(inputData, pattern).Where(text => !text.StartsWith("don't()")).ToArray();
+        string doResults = string.Join("", result);
+        IEnumerable<string> mulStrings = GetMulStrings(doResults);
 
+        int total = GetTotal(mulStrings);
+        Console.WriteLine(total);
+    }
 
-            .Split("mul")
+    private static IEnumerable<string> GetMulStrings(string inputData)
+    {
+        return inputData.Split("mul")
             .Select(static text => text.Length >= 9 ? text[..9] : text)
             .Where(static text => text.StartsWith('(') && text.Contains(')'))
             .Select(static text => text[..text.IndexOf(')')])
-            .Select(static text => text.Trim('('))
-            .ToArray();
+            .Select(static text => text.Trim('('));
+    }
 
+    private static int GetTotal(IEnumerable<string> mulStrings)
+    {
         int total = 0;
-        foreach (string text in textSplit)
+        foreach (string text in mulStrings)
         {
             string[] numberStrs = text.Split(',');
-            if (int.TryParse(numberStrs[0], out int leftNum ) && int.TryParse(numberStrs[1], out int rightNum))
+            if (!int.TryParse(numberStrs[0], out int leftNum) 
+            || !int.TryParse(numberStrs[1], out int rightNum) 
+            || leftNum > 999 || rightNum > 999)
             {
-                Console.WriteLine(leftNum + " " + rightNum);
-                if (leftNum > 999 || rightNum > 999)
-                {
-                    break;
-                }
-                total += leftNum * rightNum;
+                continue;
             }
+
+            total += leftNum * rightNum;
         }
-        // 166357705
-        Console.WriteLine(total);
+        return total;
     }
 }
