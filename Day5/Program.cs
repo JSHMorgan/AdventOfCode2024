0f26f8a5663ecc017f498aad144d943577f27be9
Jack Morgan <Jack.Morgan@mmu.ac.uk>
2024-12-05 11:02:19 +0000

    Created Day Five.

---
+++ Day5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Day5;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        string inputData = File.ReadAllText($@"{Environment.CurrentDirectory}/data/test_input.txt");
+        string[] inputArray = inputData.Split(Environment.NewLine);
+
+        SplitArray(inputArray, string.Empty, out string[] first, out string[] second);
+        foreach (string text in first)
+        {
+            Console.WriteLine(text);
+        }
+        Console.WriteLine("SECOND");
+        foreach (string text in second)
+        {
+            Console.WriteLine(text);
+        }
+    }
+
+    private static void SplitArray(string[] input, string delimiter, out string[] first, out string[] second)
+    {
+        int index = Array.IndexOf(input, delimiter);
+        first = input.Take(index - 1).ToArray();
+        second = input.Skip(index + 1).ToArray();
+    }
+}
